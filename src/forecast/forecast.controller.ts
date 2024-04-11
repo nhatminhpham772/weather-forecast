32d1442d5898778105d61eac88a467b785cfcef7
nhatminhpham772 <nhatminh772002@gmail.com>
2024-04-11 13:10:07 +0700

    [Fix]: forecast response

--- src/forecast/forecast.controller.ts
+++ src/forecast/forecast.controller.ts
@@ -1,21 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
 import { ForecastService } from './forecast.service';
 import { ApiTags } from '@nestjs/swagger';
+import AppResponse, { AppResponseInterface } from 'src/config/sections/app.response';
 
 @ApiTags('FORECAST')
 @Controller('forecast')
 export class ForecastController {
+  private res: AppResponseInterface = AppResponse
   constructor(
     private readonly forecastService: ForecastService,
   ) { }
 
   @Get('/current/:city')
-  async currentWeather(@Param('city') city: string) {
-    return await this.forecastService.currentWeather(city)
+  async currentWeather(@Param('city') city: string, @Res() res) {
+    try {
+      let data = await this.forecastService.currentWeather(city)
+      return this.res.setLib(res).setStatus(200).setBody(data).release()
+    } catch (error) {
+      return this.res.setLib(res).setStatus(400).setBody(error.toString()).release()
+    }
   }
 
   @Get('/future/:city/:date')
-  async forecastWeather(@Param('city') city: string, @Param('date') date: string) {
+  async forecastWeather(@Param('city') city: string, @Param('date') date: string, @Res() res) {
     const dateIp = new Date(date);
     const dateCr = this.forecastService.VNTime()
     
@@ -23,8 +30,18 @@ export class ForecastController {
     const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
 
     if(daysDiff <= 13)
-      return await this.forecastService.forecastWeatherFrom0To12Day(city)
+      try {
+        let data = await this.forecastService.forecastWeatherFrom0To12Day(city)
+        return this.res.setLib(res).setStatus(200).setBody(data).release()
+      } catch (error) {
+        return this.res.setLib(res).setStatus(400).setBody(error.toString()).release()
+      }
     else
-      return await this.forecastService.forecastWeatherFrom13To300Day(city, dateIp)
+      try {
+        let data = await this.forecastService.forecastWeatherFrom13To300Day(city, dateIp)
+        return this.res.setLib(res).setStatus(200).setBody(data).release()
+      } catch (error) {
+        return this.res.setLib(res).setStatus(400).setBody(error.toString()).release()
+      }
   }
 }
